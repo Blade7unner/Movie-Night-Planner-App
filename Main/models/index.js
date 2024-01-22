9724aa7f8e48e098dcdc78e684e6ff6f454470e3
yungtaco <dvhyun@gmail.com>
2024-01-22 18:51:01 -0500

    Edited /Main/models/index.js

--- Main/models/index.js
+++ Main/models/index.js
@@ -1,19 +1,13 @@
 const User = require('./User');
-const MovieNight = require('./MovieNight');
-const Invitation = require('./Invitation');
+const Project = require('./Project');
 
-User.hasMany(MovieNight, {
+User.hasMany(Project, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Invitation.belongsTo(MovieNight, {
-  foreignKey: '',
-  onDelete: 'CASCADE'
-});
-
-MovieNight.belongsTo(User, {
+Project.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, MovieNight , Invitation };
+module.exports = { User, Project };
