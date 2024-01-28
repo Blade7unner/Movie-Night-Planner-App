ab26a00a062ecd4968c9c31e221edb8a3fd2cb0b
Javier Guerra <javierguerradevelop@gmail.com>
2024-01-28 17:55:46 -0500

    adding export to server.js to schedule and send emails

--- Main/server.js
+++ Main/server.js
@@ -11,6 +11,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const { checkAndScheduleReminders } = require('./controllers/emailer');
+
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
@@ -44,3 +46,5 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+checkAndScheduleReminders();
